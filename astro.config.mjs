328b1f47322a5e13971b65077344e8831f49e10b
Neo <99268950+n30w@users.noreply.github.com>
2024-11-20 02:05:31 +0800

    chore: add fmt deps

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,10 +5,23 @@ import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
 import mdx from '@astrojs/mdx';
 import cloudflare from '@astrojs/cloudflare'
+import AutoImport from 'astro-auto-import'
+import compressor from 'astro-compressor'
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
   adapter: cloudflare();
-  integrations: [tailwind(), sitemap(), mdx()]
+  integrations: [
+    tailwind(), 
+    sitemap(), 
+
+    AutoImport({}), 
+
+    // mdx MUST come after AutoImport.
+    mdx(), 
+
+
+    // Compressor MUST come last.
+    compressor()]
 });
