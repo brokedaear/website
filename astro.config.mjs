5e9a37574151a04566317eeda68e99c5a65b354e
Neo <99268950+n30w@users.noreply.github.com>
2024-11-20 00:00:20 +0800

    build: add astro deps

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,14 @@
 // @ts-check
-// @ts-check
 import { defineConfig } from 'astro/config';
 
+import tailwind from '@astrojs/tailwind';
+import sitemap from '@astrojs/sitemap';
+import mdx from '@astrojs/mdx';
+import cloudflare from '@astrojs/cloudflare'
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  output: 'server',
+  adapter: cloudflare();
+  integrations: [tailwind(), sitemap(), mdx()]
+});
