d46e0732562dad9cb6b8a7409a739c6526d98eb4
Neo <99268950+n30w@users.noreply.github.com>
2025-01-16 00:20:20 -1000

    chore: use `netlify` not `cloudflare`

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,19 +4,20 @@ import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
 import sitemap from '@astrojs/sitemap'
 
-// Importing MDX for now creates an error. Must be an astro beta thing.
-// import mdx from '@astrojs/mdx'
-
 import cloudflare from '@astrojs/cloudflare'
 import AutoImport from 'astro-auto-import'
 import compressor from 'astro-compressor'
 import mdx from '@astrojs/mdx'
 
+import netlify from '@astrojs/netlify'
+
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://brokedaear.com',
+  // site: 'https://brokedaear.com',
   output: 'server',
-  adapter: cloudflare(),
+  adapter: netlify({
+    edgeMiddleware: true,
+  }),
   integrations: [
     tailwind(),
     sitemap(),
